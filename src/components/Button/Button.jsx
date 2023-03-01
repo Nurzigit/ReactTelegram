e5ec8d249638bf430c6f1379eadbeaba5f7ece75
Nurzhigit <nurzigitturman@gmail.com>
2023-03-01 09:14:49 +0300

    fix third err.

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,7 +3,7 @@ import './Button.css';
 
 const Button = (props) => {
     return (            
-        <button {...props} className={'button' + props.className}/>
+        <button {...props} className={'button ' + props.className}/>
     );
 };
 
