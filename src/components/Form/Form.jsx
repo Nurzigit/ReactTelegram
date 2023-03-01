5fb47455ac9683ae7ffb931e32b8e41f2c5639f1
Nurzhigit <nurzigitturman@gmail.com>
2023-03-01 09:03:32 +0300

    using useCallback

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import './Form.css';
 import { useTelegram } from './../../hooks/useTelegram';
 
@@ -7,6 +7,22 @@ const Form = () => {
     const [street, setStreet] = useState();
     const [subject, setSubject] = useState('physical');
     const {tg} = useTelegram();
+
+    const onSendData = useCallback(()=> {
+        const data = {
+            country,
+            street,
+            subject
+        }
+        tg.SendData(JSON.stringify(data));
+    }, [country, street, subject])
+
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData);
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData);
+        }
+    }, [onSendData])
 
     const onChangeCountry = (e) => {
         setCountry(e.target.value);
